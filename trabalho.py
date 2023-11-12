f71d042ed43f83844e9cb682899311ec7508ac1e
unknown <lucasrpereira29@gmail.com>
2023-11-12 13:17:15 -0300

    att

--- trabalho.py
+++ trabalho.py
@@ -2,9 +2,7 @@ import pandas as pd
 from io import StringIO #finge qie é um arquivo, mas é string.
 import matplotlib.pyplot as plt #graficos
 import numpy as np #show de bola pra trabalhar
-
-
-
+import streamlit as st #pagina
 
 unicode = ['utf-8', 'latin1', 'iso-8859-1'] # Aqui teve bug com o unicode, esse for com try catch testa esses 3 tipos diferentes.
 for unicode in unicode:
@@ -16,19 +14,26 @@ for unicode in unicode:
     except UnicodeDecodeError:
         continue
 
-
-
 def tresPrimeirosBairros():
-
     bairros = df['bairro'].value_counts()
     bairrosRepetidos = bairros.head(3).index.tolist() #os 3 quemais se repetem.
     return bairrosRepetidos
 
+def tresPioresBairros():
+    bairros = df['bairro'].value_counts()
+    pioresBairrosRepetidos = bairros.tail(3).index.tolist() #os 3 quemais se repetem.
+    return pioresBairrosRepetidos
 
 def recorrenciaBairros():
     bairros = df['bairro'].value_counts()
     bairrosRepetidos = bairros.head(3).index.tolist() #os 3 quemais se repetem.
     recorrencia = df[df['bairro'].isin(bairrosRepetidos)]['bairro'].value_counts() #aqui pegamos a quantidade de vezes que repeiu.
+    return recorrencia.values
+
+def recorrenciaPioresBairros():
+    bairros = df['bairro'].value_counts()
+    pioresBairrosRepetidos = bairros.tail(3).index.tolist() #os 3 que menos se repetem.
+    recorrencia = df[df['bairro'].isin(pioresBairrosRepetidos)]['bairro'].value_counts() #aqui pegamos a quantidade de vezes que repeiu.
     return recorrencia.values
 
 def precoMedio():
@@ -44,11 +49,43 @@ def graficoBairros():
     tamanho = recorrenciaBairros()
     preco = precoMedio()
     
-
-    ax = plt.subplots()
+    fig, ax = plt.subplots()
     ax.pie(tamanho, labels=bairros)
     ax.set_title('Analise dos bairros mais vendidos')
     ax.legend(preco,title="Preço medio em R$:",loc="best")
-    plt.show()
+    st.pyplot(fig)
 
+    st.code('''
+            def tresPrimeirosBairros():
+            bairros = df['bairro'].value_counts()
+            bairrosRepetidos = bairros.head(3).index.tolist()
+            return bairrosRepetidos
+            ''')
+    
+    st.code('''
+            def recorrenciaBairros():
+            bairros = df['bairro'].value_counts()
+            bairrosRepetidos = bairros.head(3).index.tolist() 
+            recorrencia = df[df['bairro'].isin(bairrosRepetidos)]['bairro'].value_counts() 
+            return recorrencia.values
+            ''')
+    st.code('''
+            def precoMedio():
+            bairros = df['bairro'].value_counts()
+            bairrosRepetidos = bairros.head(3).index.tolist() 
+            colunaPreco = df[df['bairro'].isin(bairrosRepetidos)] 
+            preco = colunaPreco.groupby('bairro')['preco'].mean()
+            preco = preco[bairrosRepetidos].values.astype(int)
+            return preco
+            ''')
+
+
+    bairros2 = tresPioresBairros()
+    tamanho2 = recorrenciaPioresBairros()
+    fig2, ax2 = plt.subplots()
+    ax2.pie(tamanho2, labels=bairros2)
+    ax2.set_title('Analise dos bairros menos vendidos')
+    st.pyplot(fig2)
+
+st.title("analise de dados")
 graficoBairros()
