718941eaeabe75c6ddb1248f51a44772ac75ac39
unknown <lucasrpereira29@gmail.com>
2023-11-13 17:22:44 -0300

    leo

--- trabalho.py
+++ trabalho.py
@@ -1,18 +1,9 @@
 import pandas as pd
-from io import StringIO #finge qie é um arquivo, mas é string.
 import matplotlib.pyplot as plt #graficos
 import numpy as np #show de bola pra trabalhar
 import streamlit as st #pagina
 
-unicode = ['utf-8', 'latin1', 'iso-8859-1'] # Aqui teve bug com o unicode, esse for com try catch testa esses 3 tipos diferentes.
-for unicode in unicode:
-    try:
-        with open('kgz7s-3aihd.csv', 'rb') as file:
-            auxiliar = file.read()
-            auxiliar = auxiliar.decode(unicode)
-            df = pd.read_csv(StringIO(auxiliar)) #aqui fingimos que o auxiliar é um arquivo e criamos um dataframe.
-    except UnicodeDecodeError:
-        continue
+df = pd.read_csv('dados_imob.csv')
 
 def tresPrimeirosBairros():
     bairros = df['bairro'].value_counts()
@@ -112,7 +103,69 @@ def graficoGaragem():
                 ax.bar(labelsGaragem, valoresGaragem)
             ''')
 
+def preco_por_area():
+    df['preco_m2'] = df['preco'] / df['m2'] #Calcula o preço por metro quadrado
+    
+    bairros_repetidos = df['bairro'].value_counts().head(10).index #obtém os 10 bairros mais repetidos
+
+    df_filtrado = df[df['bairro'].isin(bairros_repetidos)] #filtra o df para incluir apenas os bairros mais repetidos
+
+    media_por_bairro = df_filtrado.groupby('bairro')['preco_m2'].mean() #Agrupa pelos bairros e calcula a média do preço por metro quadrado
+
+    # Cria um novo DataFrame com os resultados
+    resultado = pd.DataFrame({
+        'Bairros': media_por_bairro.index,
+        'Preço/m2': media_por_bairro.values
+    })
+
+    resultado['Quantidade'] = df['bairro'].value_counts().reindex(resultado['Bairros']).values #Adiciona a coluna de quantidade de imóveis nos bairros
+
+    resultado = resultado.sort_values(by='Quantidade', ascending=False) #Ordena pelos bairros mais repetidos
+
+    # Remove a coluna de quantidade para ter o resultado final
+    #resultado = resultado.drop('Quantidade', axis=1)
+    
+    resultado['Preço/m2'] = resultado['Preço/m2'].astype(int)# arredonda para duas casas decimais
+
+    resultado = resultado.reset_index(drop=True) #Reinicia os índices do DataFrame
+
+    #Código para deixar "Tabela de Preço/m2 em negrito e centralizado"
+    #st.markdown("<h1 style='text-align: center; color: black;'><b>Tabela de Resultados</b></h1>", unsafe_allow_html=True)
+
+    st.write("Tabela de preço por metro quadrado")
+
+    st.table(resultado) #exibe a tabela na página
+
+    st.code('''
+            def preco_por_area():
+                df['preco_m2'] = df['preco'] / df['m2'] #Calcula o preço por metro quadrado
+                bairros_repetidos = df['bairro'].value_counts().head(10).index #obtém os 10 bairros mais repetidos
+            
+                df_filtrado = df[df['bairro'].isin(bairros_repetidos)] #filtra o df para incluir apenas os bairros mais repetidos
+
+                media_por_bairro = df_filtrado.groupby('bairro')['preco_m2'].mean() #Agrupa pelos bairros e calcula a média do preço por metro quadrado
+
+                # Cria um novo DataFrame com os resultados
+                resultado = pd.DataFrame({
+                    'Bairros': media_por_bairro.index,
+                    'Preço/m2': media_por_bairro.values
+                })
+
+                resultado['Quantidade'] = df['bairro'].value_counts().reindex(resultado['Bairros']).values #Adiciona a coluna de quantidade de imóveis nos bairros
+
+                resultado = resultado.sort_values(by='Quantidade', ascending=False) #Ordena pelos bairros mais repetidos
+
+                # Remove a coluna de quantidade para ter o resultado final
+                #resultado = resultado.drop('Quantidade', axis=1)
+    
+                resultado['Preço/m2'] = resultado['Preço/m2'].astype(int)# arredonda para duas casas decimais
+
+                st.table(resultado)
+    ''')
+
+
 st.title("analise de dados")
 
 graficoBairros()
 graficoGaragem()
+preco_por_area()
